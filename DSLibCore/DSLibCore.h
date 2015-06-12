b1466bd5fcd0321c4fa1126e5355e7bb0303b99b
Alexander Belyavskiy <diejmon@gmail.com>
2015-06-12 17:10:50 +0300

    Framework Target added

---
+++ DSLibCore/DSLibCore.h
@@ -0,0 +1,24 @@
+//
+//  DSLibCore.h
+//  DSLibCore
+//
+//  Created by Alexander Belyavskiy on 6/12/15.
+//  Copyright (c) 2015 Alexander Belyavskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSLibCore.
+FOUNDATION_EXPORT double DSLibCoreVersionNumber;
+
+//! Project version string for DSLibCore.
+FOUNDATION_EXPORT const unsigned char DSLibCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSLibCore/PublicHeader.h>
+#import <DSLibCore/DSCFunctions.h>
+#import <DSLibCore/DSCodingTransformer.h>
+#import <DSLibCore/DSConstants.h>
+#import <DSLibCore/DSQueue.h>
+#import <DSLibCore/DSReachability.h>
+#import <DSLibCore/DSTimeFunctions.h>
+#import <DSLibCore/NSString+Extras.h>
