a801f18777696a4e844bcf3b0a7ffd0aa50727c4
Alexander Belyavskiy <diejmon@gmail.com>
2016-05-14 10:56:53 +0300

    Make isScreenSizeHigherThanIPhone4 a function

--- Pod/Classes/DSMacros.m
+++ Pod/Classes/DSMacros.m
@@ -12,3 +12,6 @@ BOOL isIPhone4Screen(void) {
   return [UIScreen mainScreen].bounds.size.height == 480 && [UIScreen mainScreen].bounds.size.width == 320;
 }
 
+BOOL isScreenSizeHigherThanIPhone4(void) {
+  return ([[UIScreen mainScreen] preferredMode].size.height > 480 * [[UIScreen mainScreen] scale] && !isIPadIdiom());
+}
