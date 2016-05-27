b61e1202873120f6c723607839ac109c0942923c
Alexander Belyavskiy <diejmon@gmail.com>
2016-05-27 09:17:52 +0300

    [Docs] Add docs to ApplicationProxy protocol

--- Pod/Classes/ApplicationProxy.swift
+++ Pod/Classes/ApplicationProxy.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 
-@objc
-public protocol ApplicationProxy {
+/// A protocol for UIApplication. 
+/// It was created to abstract UIApplication class which isn't available in extensions from frameworks which uses UIApplication interface.
+@objc public protocol ApplicationProxy {
   func openURL(url: NSURL) -> Bool
   var keyWindow: UIWindow? { get }
   func beginIgnoringInteractionEvents()
